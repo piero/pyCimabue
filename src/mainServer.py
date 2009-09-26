7859a4f951502076930990bf3197c0a09c35f890
Piero Piero <git@pieroland.net>
2009-09-26 15:34:55 +0100

    Formalised main execution

--- src/mainServer.py
+++ src/mainServer.py
@@ -63,38 +63,39 @@ def query_server_role(server_name, host_and_port):
 
 ### EXECUTION STARTS HERE ###
 
-# Parse command-line args
-listen_ip, listen_port = None, None
-
-if len(sys.argv) == 2:
-	listen_port = int(sys.argv[1])
-elif len(sys.argv) == 3:
-	listen_ip = sys.argv[1]
-	listen_port = int(sys.argv[2])
-
-
-# Create Server, Listener and start
-server = Server()
-
-if listen_ip != None:
-	listener = Listener(executioner=server, host=listen_ip, port=listen_port)
-elif listen_port != None:
-	listener = Listener(executioner=server, port=listen_port)
-else:
-	listener = Listener(executioner=server)
+if __name__=="__main__":
+	# Parse command-line args
+	listen_ip, listen_port = None, None
 	
-listener.start()
-
-
-# Set Server server_role
-(server_role, master) = query_server_role(server.get_name(), listener.get_host_and_port())
-server.set_role(server_role, master)
-
-
-# Sleep
-while listener.is_alive():
-	time.sleep(1)
-
-# Exit
-print 'Bye :)'
+	if len(sys.argv) == 2:
+		listen_port = int(sys.argv[1])
+	elif len(sys.argv) == 3:
+		listen_ip = sys.argv[1]
+		listen_port = int(sys.argv[2])
+	
+	
+	# Create Server, Listener and start
+	server = Server()
+	
+	if listen_ip != None:
+		listener = Listener(executioner=server, host=listen_ip, port=listen_port)
+	elif listen_port != None:
+		listener = Listener(executioner=server, port=listen_port)
+	else:
+		listener = Listener(executioner=server)
+		
+	listener.start()
+	
+	
+	# Set Server server_role
+	(server_role, master) = query_server_role(server.get_name(), listener.get_host_and_port())
+	server.set_role(server_role, master)
+	
+	
+	# Sleep
+	while listener.is_alive():
+		time.sleep(1)
+	
+	# Exit
+	print 'Bye :)'
 
