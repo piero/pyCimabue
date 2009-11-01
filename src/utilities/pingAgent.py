3909ed211208e5aa68667364880268d18f2a6252
Piero Piero <git@pieroland.net>
2009-11-01 17:48:19 +0000

    Use logging facilities in PingAgent

--- src/utilities/pingAgent.py
+++ src/utilities/pingAgent.py
@@ -32,17 +32,19 @@ class PingAgent(threading.Thread):
 						
 						if k != self.__caller.get_role().backup[0]:
 							del self.__caller.get_role().servers[k]
-							print "Removed %s (%d servers left)" % (k, len(self.__caller.get_role().servers))
+							self.__caller.output("Removed %s (%d servers left)" % (k, len(self.__caller.get_role().servers)),
+												logging.WARNING)
 							self.__caller.get_role().sync_server_list()
 						else:
 							del self.__caller.get_role().backup
 							self.__caller.get_role().backup = None
-							print 'Backup left (%s)!' % k
+							self.__caller.output("Backup left (%s)!" % k,
+												logging.WARNING)
 							# TODO: Start Backup rescue procedure!
 			
 			else:
 				master = self.__caller.get_role().get_master()
-				print "Pinging Master: %s (%s:%d)" % (master[0], master[1][0], master[1][1])
+				self.__caller.output("Pinging Master: %s (%s:%d)" % (master[0], master[1][0], master[1][1]))
 				msg = PingMessage(priority=1)
 				msg.serverSrc = self.__caller.get_name()
 				msg.serverDst = master[0]
@@ -50,17 +52,17 @@ class PingAgent(threading.Thread):
 
 				# Master not replying: trigger rescue procedure
 				if reply == None:
-					print "Master %s not replying!" % master[0]
+					self.__caller.output(("Master %s not replying!" % master[0]), logging.WARNING)
 					
 					if self.__caller.get_role().name == self.__caller.BACKUP:
 						self.__caller.get_role().elect_new_master()
 				
 				# Master has changed, make it know we're here
 				elif reply.type == "ErrorMessage":
-					print "Master %s doesn't know me" % master[0]
+					self.__caller.output(("Master %s doesn't know me" % master[0]), logging.WARNING)
 					self.__caller.query_role()
 			
-		print "[x] PingAgent"
+		self.__caller.output("[x] PingAgent", logging.INFO)
 	
 	
 	def kill(self):
